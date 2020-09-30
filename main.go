fc53d9f2fbfa4e6fe38d7c02ce20bf27c00011b5
hong <785131182@qq.com>
2020-09-30 10:14:18 +0800

    fix to flag

--- main.go
+++ main.go
@@ -17,16 +17,19 @@ func main() {
 	flags.StringVar(&c.pass, "p", "", "pass")
 	if err := flags.Parse(os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
+		flags.Usage()
 		return
 	}
 
 	if err := initFile(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
+		flags.Usage()
 		return
 	}
 
 	if err := c.execCommand(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
+		flags.Usage()
 		return
 	}
 }
